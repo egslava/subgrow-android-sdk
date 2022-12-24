626a8db46b08249d3e429397c02e0ebe0607be65
Slava <egslava@gmail.com>
2022-12-24 21:53:57 +0300

    updating ci/cd scripts

--- .github/workflows/scripts/tutorial_build_release.sh
+++ .github/workflows/scripts/tutorial_build_release.sh
@@ -1,4 +1,6 @@
 set -e
+echo $GITHUB_REPOSITORY
+echo $GITHUB_REPOSITORY_GITHUB_REPOSITORY_OWNER
 github_repo_name=`python -c "import sys; print(sys.argv[1].split('/')[1])" $GITHUB_REPOSITORY`
 $GITHUB_RUN_ID
 export subgrow_sdk_maven_repo="com.github.$GITHUB_REPOSITORY_OWNER:$github_repo_name:0.0.$LAST_GITHUB_TAG"
