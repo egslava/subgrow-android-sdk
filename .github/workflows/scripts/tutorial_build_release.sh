e8b086ffa4c06a90dffe6ea548208ebc6eb77c06
Slava <egslava@gmail.com>
2022-12-24 21:56:37 +0300

    updating ci/cd scripts

--- .github/workflows/scripts/tutorial_build_release.sh
+++ .github/workflows/scripts/tutorial_build_release.sh
@@ -2,7 +2,7 @@ set -e
 echo $GITHUB_REPOSITORY
 echo $GITHUB_REPOSITORY_GITHUB_REPOSITORY_OWNER
 github_repo_name=`python3 -c "import sys; print(sys.argv[1].split('/')[1])" $GITHUB_REPOSITORY`
-$GITHUB_RUN_ID
+echo 'run id: '$GITHUB_RUN_ID
 export subgrow_sdk_maven_repo="com.github.$GITHUB_REPOSITORY_OWNER:$github_repo_name:0.0.$LAST_GITHUB_TAG"
 
 ./gradlew :tutorial:bundleRelease -Dhttp.socketTimeout=600000 -Dhttp.connectionTimeout=600000
