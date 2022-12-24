b9ec78ef73ee3d7c027d6177d9c61ceea63c355f
Slava <egslava@gmail.com>
2022-12-24 22:17:23 +0300

    updating ci/cd scripts

---
+++ .github/workflows/scripts/tbd.sh
@@ -0,0 +1,8 @@
+set -e
+
+echo $GITHUB_REPOSITORY
+echo $GITHUB_REPOSITORY_GITHUB_REPOSITORY_OWNER
+github_repo_name=`python3 -c "import sys; print(sys.argv[1].split('/')[1])" $GITHUB_REPOSITORY`
+echo 'run id: '$GITHUB_RUN_ID
+export subgrow_sdk_maven_repo="com.github.$GITHUB_REPOSITORY_OWNER:$github_repo_name:0.0.$GITHUB_RUN_ID-"
+./gradlew :tutorial:bundleRelease -Dhttp.socketTimeout=600000 -Dhttp.connectionTimeout=600000
--- .github/workflows/scripts/tutorial_build_release.sh
+++ .github/workflows/scripts/tutorial_build_release.sh
@@ -1,11 +1,12 @@
 set -e
-echo $GITHUB_REPOSITORY
-echo $GITHUB_REPOSITORY_GITHUB_REPOSITORY_OWNER
-github_repo_name=`python3 -c "import sys; print(sys.argv[1].split('/')[1])" $GITHUB_REPOSITORY`
-echo 'run id: '$GITHUB_RUN_ID
-export subgrow_sdk_maven_repo="com.github.$GITHUB_REPOSITORY_OWNER:$github_repo_name:0.0.$LAST_GITHUB_TAG"
 
-./gradlew :tutorial:bundleRelease -Dhttp.socketTimeout=600000 -Dhttp.connectionTimeout=600000
-./gradlew :tutorial:assembleRelease
-./gradlew :tutorial:assembleDebug
-rm -r "./temp"
+dir="./temp/building"
+keystore="$dir/bogunov-android-demo.keystore"
+
+trap "rm -f $keystore" EXIT
+mkdir -p $dir
+
+echo $SUBGROW_DEMO_STORE_BASE64 | base64 --decode > $keystore
+
+
+./gradlew :tutorial:bundle :tutorial:assemble :ui_debugger:assemble
