6f9727082a775304298bb2dd0d3cda3ff76ad0de
Slava <egslava@gmail.com>
2022-12-25 23:04:44 +0300

    fix up

--- .github/workflows/scripts/_0_configure.sh
+++ .github/workflows/scripts/_0_configure.sh
@@ -3,7 +3,7 @@ set -e
 dir="./temp/building"
 keystore="$dir/bogunov-android-demo.keystore"
 
-trap "rm -f ./temp" EXIT
+trap "rm -rf ./temp" EXIT
 mkdir -p $dir
 
 echo $SUBGROW_DEMO_STORE_BASE64 | base64 --decode > $keystore
